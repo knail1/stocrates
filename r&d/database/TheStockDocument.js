7ecddacb912fc713f54168ee800b5c6db38260d7
knail1 <knail1@live.com>
2013-12-28 21:18:54 -0600

    updated the JSON stock document, added current stock price
    updated the JSON stock document, added current stock price

--- r&d/database/TheStockDocument.js
+++ r&d/database/TheStockDocument.js
@@ -12,6 +12,7 @@
 		"source" : "Barrons"
 		"dateWhenPredicted" : "8/8/2013" 
 		"futurePredictedDate" : "6/6/2016", // bson date time (check blog project, ISODate("2012-11-20T05:05:15.229Z")) if future date doesn't exist this was a sh*tty recommendation, rate the analyst accordingly
+		"priceWhenPredicted" : 21, // price of the stock when at the time of this predication
 		"futurePredictedPrice" : 46, // if future price doesn't exist this was an even more sh*tty recommendation, rate analyst accordingly
 		"wasDateCalculated" : 1 // 1 is yes calculated by stAR, 0 is no, the analyst provided this date
 		"wasPriceCalculated" : 1 // 1 is yes, 0 is no
@@ -29,6 +30,7 @@
 		"source" : "Barrons"
 		"dateWhenPredicted" : "3/4/2013" 
 		"futurePredictedDate" : "3/6/2016", // bson date time, if future date doesn't exist this was a sh*tty recommendation, rate the analyst accordingly
+		"priceWhenPredicted" : 21, // price of the stock when at the time of this predication
 		"futurePredictedPrice" : 26, // if future price doesn't exist this was an even more sh*tty recommendation, rate analyst accordingly
 		"wasDateCalculated" : 1 // 1 is yes calculated by stAR, 0 is no, the analyst provided this date
 		"wasPriceCalculated" : 0 // 1 is yes it was calculated by stAR, 0 is no, it was precisely provided by analyst
@@ -46,6 +48,7 @@
 		"source" : "Barrons"
 		"dateWhenPredicted" : "3/4/2013" 
 		"futurePredictedDate" : "3/6/2016", // bson date time, if future date doesn't exist this was a sh*tty recommendation, rate the analyst accordingly
+		"priceWhenPredicted" : 21, // price of the stock when at the time of this predication
 		"futurePredictedPrice" : 96, // if future price doesn't exist this was an even more sh*tty recommendation, rate analyst accordingly
 		"wasDateCalculated" : 0 // 1 is yes calculated by stAR, 0 is no, the analyst provided this date
 		"wasPriceCalculated" : 1 // 1 is yes it was calculated by stAR, 0 is no, it was precisely provided by analyst
